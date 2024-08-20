d34f0aabbddbf6dcc33bc95d4ee55e3588178a6c
Florian Duport <f.duport@programisto.fr>
2024-08-20 16:29:54 +0200

    add domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/space-station-white.ico',
 
   // Set the production url of your site here
-  url: 'https://main-bvxea6i-xcuf3652hubp4.fr-4.platformsh.site/',
+  url: 'https://endurancejs.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
