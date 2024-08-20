61a66e62da7cafa4b4a7e74a879b81ea9a2183fb
Florian Duport <f.duport@programisto.fr>
2024-08-20 16:56:25 +0200

    rollback domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/space-station-white.ico',
 
   // Set the production url of your site here
-  url: 'https://endurancejs.com',
+  url: 'https://endurancejs.com.main-bvxea6i-xcuf3652hubp4.fr-4.platformsh.site/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
