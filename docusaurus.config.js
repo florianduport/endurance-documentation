b3ab07a3adbc85be2ddf93e5eaa5e7b9520e7b95
Florian Duport <f.duport@programisto.fr>
2024-08-26 09:59:38 +0200

    add custom gpt link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,6 +76,7 @@ const config = {
             label: 'Documentation',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
+          {to: 'https://chatgpt.com/g/g-nSGsf6hh5-endurance-gpt', label: 'Custom GPT', position: 'left'},
           {
             href: 'https://github.com/florianduport/endurance',
             label: 'GitHub',
